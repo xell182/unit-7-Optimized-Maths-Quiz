2c158c01164d28b7d117db1e2fe99ea70c461b88
xell182 <68129354+xell182@users.noreply.github.com>
2022-05-24 11:36:52 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What's 1+0?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What's 1+1?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What's 1+2?",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What's 1+3?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What's 1+4?",
+    answer: "5",
+    options: [
+      "5",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What's 1+5?",
+    answer: "6",
+    options: [
+      "5",
+      "6",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What's 1+6?",
+    answer: "7",
+    options: [
+      "5",
+      "2",
+      "7",
+      "4"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What's 1+7?",
+    answer: "8",
+    options: [
+      "5",
+      "2",
+      "3",
+      "8"
+    ]
+  },
+    {
+    numb: 9,
+    question: "What's 1+8?",
+    answer: "9",
+    options: [
+      "5",
+      "2",
+      "9",
+      "4"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What's 1+9?",
+    answer: "10",
+    options: [
+      "5",
+      "10",
+      "3",
+      "4"
+    ]
+  }
+]
